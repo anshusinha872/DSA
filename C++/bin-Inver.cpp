7a6e3874d57b0dfea185103c54bc6198c66b7c86
Anshu Sinha <anshusinha872@gmail.com>
2021-09-30 09:45:29 +0530

    fixed error

--- C++/bin-Inver.cpp
+++ C++/bin-Inver.cpp
@@ -1,46 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-int minGCD(int arr[],int n,int even_number,int odd_number){
-    ll count = INT_MAX;
-    
-    ll curr_count = 0;
-    if(n==even_number)
+int OddGCD(int n)
+{
+    int odd = 0, even = 0, count = 0, arr[n];
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < n;i++){
-            curr_count = 0;
-            while(arr[i]!=1 && arr[i]>0){
-                arr[i] = arr[i] / 2;
-                curr_count++;
-                cout << curr_count << " curre" << endl;
-            }
-            cout << endl;
-            if(arr[i]==0)
-                curr_count = 0;
-            if(curr_count<count)
-                count = curr_count;
-            
-        }
-        return count;
+        cin >> arr[i];
+        if (arr[i] % 2 != 0)
+            odd++;
+        else
+            even++;
     }
-    return 0;
-}
-int main(){
-    ll t;
-    cin >> t;
-    while(t--){
-        ll n;
-        ll even_number = 0, odd_number = 0;
-        cin >> n;
-        int arr[n];
-        for (ll i = 0; i < n;i++){
-            cin >> arr[i];
-            if(arr[i]%2==0)
-                even_number++;
-            else
-                odd_number++;
+    int min = INT_MAX;
+
+    if (odd == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+
+            while (1)
+            {
+                if (arr[i] == 0)
+                {
+                    count = 0;
+                    break;
+                }
+                else if (arr[i] % 2 != 0)
+                    break;
+                else
+                {
+                    arr[i] = arr[i] / 2;
+                    count++;
+                }
+            }
+            if (min > count && count != 0)
+                min = count;
+            count = 0;
         }
-        cout <<minGCD(arr, n,even_number,odd_number) << endl;
+    }
+    if (min == INT_MAX)
+        return 0;
+    else
+        return min;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << OddGCD(n) << endl;
     }
     return 0;
 }
--- C++/fixfix.cpp
+++ C++/fixfix.cpp
@@ -1,34 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-void fixfix(int n,int k){
-    if(n-k==1){
+void fixfix(int n, int k)
+{
+    if (n - k == 1)
+    {
         cout << "-1";
         return;
     }
-    else if(n==1 && k==1){
-        cout << "-1";
-        return;
+    else if (n == k)
+    {
+        {
+            for (int i = 1; i <= n; i++)
+                cout << i << " ";
+            return;
+        }
     }
-    else{
-        for (int i = 1; i <= k;i++){
+    else
+    {
+        for (int i = 1; i <= k; i++)
+        {
             cout << i << " ";
         }
-        for (int i = n; i >= k + 1;i--){
+        cout<<n<<" ";
+         for (int i = k + 1; i < n; i++)
             cout << i << " ";
-        }
         return;
     }
-
 }
-int main(){
+int main()
+{
     ll t;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         ll n, k;
         cin >> n >> k;
         fixfix(n, k);
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
